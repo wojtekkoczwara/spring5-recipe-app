3a853ef68a30ae3bae85701ab6fe2332ab8bff9d
Wojciech Koczwara <61041670+wojtekkoczwara@users.noreply.github.com>
2024-09-25 08:47:44 +0200

    mockito tests added

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.controllers;
+
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.services.RecipeService;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPageReturnTest() {
+        String returnString = "index";
+        String indexReturnString = indexController.getIndexPage(model);
+        Assert.assertEquals(returnString, indexReturnString);
+
+        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
+        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.eq("recipes"), Mockito.anySet());
+    }
+}
